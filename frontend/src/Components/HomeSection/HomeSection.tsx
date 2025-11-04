43258701484aaa5c0f373757298a50bf45657e22
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-11-04 21:19:00 +0545

    form make for adding posts

--- frontend/src/Components/HomeSection/HomeSection.tsx
+++ frontend/src/Components/HomeSection/HomeSection.tsx
@@ -1,10 +1,96 @@
+import { Avatar, Button } from "@mui/material";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import ImageIcon from "@mui/icons-material/Image";
+import { useState } from "react";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
+const validationSchema = Yup.object().shape({
+  content: Yup.string().required("Tweet text is required"),
+});
+
 export default function HomeSection() {
+  const [uploadingImage, setUploadingImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  const formik = useFormik({
+    initialValues: {
+      content: "",
+      image: "",
+    },
+    onSubmit: handleSubmit,
+    validationSchema,
+  });
+
+  const handleSelectImage = (event) => {
+    setUploadingImage(true);
+    const imgUrl = event.target.files[0];
+    formik.setFieldValue("image", imgUrl);
+    setSelectedImage(imgUrl);
+    setUploadingImage(false);
+  };
   return (
     <div className="space-y-5">
       <section>
         <h1 className="py-5 text-xl font-bold opacity-90">Homw</h1>
       </section>
-      <section></section>
+      <section className={`pb-10  `}>
+        <div className="flex  space-x-5 ">
+          <Avatar alt="username" src="" />
+
+          <div className="w-full ">
+            <form onSubmit={formik.handleSubmit}>
+              <div>
+                <input
+                  type="text"
+                  name="content"
+                  placeholder="What is happening??"
+                  className={` border-none outline-none text-xl bg-transparent`}
+                  {...formik.getFieldProps("content")}
+                />
+                {formik.errors.content && formik.touched.content && (
+                  <span className="text-red-500 ">{formik.errors.content}</span>
+                )}
+              </div>
+              {/* <div>
+                <img src="" alt="" />
+              </div> */}
+              <div className="flex justify-between items-center mt-5 ">
+                <div className="flex space-x-5 items-center ">
+                  <label className="flex items-center space-x-2 rounded-md cursor-pointer ">
+                    <ImageIcon className="text-[#id9bg0]" />
+                    <input
+                      type="file"
+                      name="imageFile"
+                      className="hidden"
+                      onChange={handleSelectImage}
+                    />
+                  </label>
+                  <FmdGoodIcon className="text-[#1d9bf0]" />
+                  <TagFacesIcon className="text-[#1d9bf0]" />
+                </div>
+                <div>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{
+                      width: "100%",
+                      borderRadius: "20px",
+                      py: "8px",
+                      px: "20px",
+                      bgcolor: "1e88e5",
+                    }}
+                  >
+                    Tweet
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
